78fdfc41b3137586710765c2fcaafd50747ad166
Ravi Sahay <ravi.sahay@nihilent.com>
2020-09-26 13:32:52 +0530

    Heroku Build

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/koruassignment'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/koruassignment/index.html'));
+});
